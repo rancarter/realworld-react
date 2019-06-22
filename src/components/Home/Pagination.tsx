7bf1448e83c3097a1e787a72fa5cee7f22418187
vitalii.korchak <vitalii.korchak@gmail.com>
2019-06-22 09:54:41 +0300

    Refactoring

---
+++ src/components/Home/Pagination.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface Props {
+  limit: number,
+  articlesCount: number,
+  currentPage: number,
+  onPageClick: (page: number) => void,
+}
+
+const Pagination: React.FC<Props> = ({
+  limit,
+  articlesCount,
+  currentPage,
+  onPageClick,
+}) => {
+  const pagesCount = articlesCount / limit;
+  const pages = [];
+
+  for (let i = 1; i <= pagesCount; i += 1) {
+    pages.push(
+      <li
+        key={i}
+        className={classNames('page-item', { 
+          active: i === currentPage,
+        })}
+      >
+        <a
+          href=""
+          className="page-link"
+          onClick={(event) => {
+            event.preventDefault();
+            onPageClick(i)
+          }}
+        >
+          {i}
+        </a>
+      </li>
+    );
+  }
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
